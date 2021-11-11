50d4e8b31af4c4c3ad34395129b43da737fa9733
Diego Ferreira <diegodejesusferreira2@gmail.com>
2021-11-10 23:18:53 -0300

    chore: add typeORM

---
+++ src/modules/users/entities/User.ts
@@ -0,0 +1,23 @@
+import { v4 as uuidv4 } from "uuid"
+
+class User {
+    id: string;
+
+    name: string;
+
+    nickName: string;
+
+    email: string;
+
+    password: string;
+
+    created_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuidv4();
+        }
+    }
+}
+
+export { User }
