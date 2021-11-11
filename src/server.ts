6d9cad1f16912455797c6fa37eb80ed8e1828651
Diego Ferreira <diegodejesusferreira2@gmail.com>
2021-11-10 21:29:47 -0300

    Add ESlint and Prettier

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,7 @@
-import express, { json, request, response } from "express"
+import express from 'express';
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello World" })
-})
+app.use(express.json());
 
-app.listen(3333, () => console.log("Server is running"))
+app.listen(3333, () => console.log('Server is running'));
